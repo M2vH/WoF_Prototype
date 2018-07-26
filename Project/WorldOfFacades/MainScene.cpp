96eedc506ad5684e099a18a936761d1e4779bffe
Marco M. von Hagen <M2vH@users.noreply.github.com>
2018-07-26 11:13:27 +0200

    added world background

--- Project/WorldOfFacades/MainScene.cpp
+++ Project/WorldOfFacades/MainScene.cpp
@@ -24,6 +24,14 @@ void GMainScene::Init()
 	// keep background in camera;
 	pBackground->SetInWorld(false);
 
+	// create a worldspace background
+	CTexturedObject* pBackgroundWorld = new CTexturedObject(
+		SVector2(0, 0),	// pos of object in World
+		SVector2(3840, 720),	// size; if 0,0 will be set to texture size
+		CEngine::Get()->GetRenderer(),
+		"Texture/World/T_world_tree_3840x720.png"
+	);
+	
 	// create Paulchen Panther textured object
 	CTexturedObjectSlide* pObj = new CTexturedObjectSlide(SVector2(100, 45), SVector2(32, 32),
 		CEngine::Get()->GetRenderer(), "PP.png");
@@ -45,8 +53,12 @@ void GMainScene::Init()
 #pragma region Adding objects to Lists
 					
 	// SceneObjects
+	// add worldbackground to SceneObject
+	CEngine::Get()->GetCM()->AddSceneObject(pBackgroundWorld);
+
 	// add background to SceneObject-List
 	CEngine::Get()->GetCM()->AddSceneObject(pBackground);
+
 
 	// PersistantObjects
 	// add object to cm
