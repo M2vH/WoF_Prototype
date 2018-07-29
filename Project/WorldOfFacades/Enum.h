1d8edf37375103b49bc4a42751b36aced6484fab
Marco M. von Hagen <M2vH@users.noreply.github.com>
2018-07-29 18:25:26 +0200

    problem with enums

--- Project/WorldOfFacades/Enum.h
+++ Project/WorldOfFacades/Enum.h
@@ -27,25 +27,25 @@ enum EEmotionType
 /// </summary>
 enum ENPCState
 {
-	NONE	= 0,
-	VISITED = 1 << 0,
-	DONE	= 1 << 1
+	// NONE,	// = 0,
+	VISITED,// = 1, // << 0,
+	DONE	//= 1 // << 1
 };
 
 /// <summary>
 /// The flagged state a Player is in.
 /// </summary>
 // ToDo (m2vh) Question: Values MUST be base of 2 (0,1,2,4,8...) ???
-[Flags]
+
 enum EPlayerState
 {
-	NONE = 0,	// for completeness
-	MOVE = 1 << 0, // Player can move
-	BLOCKED = 1 << 1,	// Player can't move
+	// NONE = 0,	// for completeness
+	IDLE = 1, // << 0, // Player can move
+	BLOCKED = 2, // << 1,	// Player can't move
 						//	while dialog is displayed
-	AT_ITEM = 1 << 2,	// Item is in reach
-	AT_NPC = 1 << 3,	// NPC in reach
-	VISIT_NPC = 1 << 4	// at NPC; block the move
+	AT_ITEM = 4,	// Item is in reach
+	AT_NPC = 8,	// NPC in reach
+	VISIT_NPC = 16 // << 4	// at NPC; block the move
 	
 	// is it possible to do this:
 	// VISIT_NPC = BLOCKED | 5;
@@ -55,11 +55,11 @@ enum EPlayerState
 
 enum EItemState
 {
-	NONE = 0,	// for completeness
-	ACTIVE = 1 << 0,
-	FOUND = 1 << 1,
-	USED = 1 << 2, // still in inventory list, but invisible
-	CONSUMED = 1 << 3	// -> NPCState::DONE
+	// NONE = 0,	// for completeness
+	ACTIVE = 1, // << 0,
+	FOUND = 2, // << 1,
+	USED = 4, //1 << 2, // still in inventory list, but invisible
+	CONSUMED = 8 // 1 << 3	// -> NPCState::DONE
 };
 
 // ToDo (m2vh) Create WorldStates
