5095949733517c29811e2707eec317fadd8b24ab
Marco M. von Hagen <M2vH@users.noreply.github.com>
2018-07-30 17:56:17 +0200

    added comment to file;

--- Project/WorldOfFacades/EnumItemState.h
+++ Project/WorldOfFacades/EnumItemState.h
@@ -6,12 +6,10 @@
 enum EItemState
 {
 	//NONE = 0,	// for completeness
-	ACTIVE = 1, // << 0,
-	FOUND = 2, // << 1,
-	USED = 4, //1 << 2, // still in inventory list, but invisible
+	ACTIVE = 1, // << 0,	// item is in world
+	FOUND = 2, // << 1,		// player has put it in inventory
+	USED = 4, //1 << 2,		// player put item in house
+							// still in inventory list, but invisible
 	CONSUMED = 8 // 1 << 3	// -> NPCState::DONE
 };
 
-// ToDo (m2vh) Create WorldStates
-// an Enum representing the states of escalation
-// WorldState should have effect on darkness of background- and foreground-textures
